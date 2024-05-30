0c2498c34ccd9b3d994490a7d27d6e90c5e61632
ChrisShep98 <shepardchristopher1@gmail.com>
2024-05-29 20:21:16 -0400

    created songVersions schema

---
+++ src/server/models/SongVersions.ts
@@ -0,0 +1,37 @@
+import mongoose, { Schema, models } from "mongoose";
+
+const songVersionSchema = new Schema(
+  {
+    songName: {
+      type: String,
+      required: true,
+    },
+    date: {
+      type: String,
+      required: true,
+    },
+    venueName: {
+      type: String,
+      required: true,
+    },
+    venueLocation: {
+      type: String,
+      required: true,
+    },
+    userWhoPosted: {
+      type: String,
+      required: true,
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+
+// or statement below says if already have the model then return the song and if you don't have then it will create a new model
+const SongVersions =
+  models.SongVersions || mongoose.model("SongVersions", songVersionSchema);
+
+export default SongVersions;
