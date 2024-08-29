c5729444dfe8e6370063cb5467505d31c3768294
ChrisShep98 <shepardchristopher1@gmail.com>
2024-08-29 12:59:23 -0400

    dynamic routes fixed

---
+++ src/app/profile/[profile]/page.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Nav from "@/app/components/Nav";
+import ProfileDetails from "@/app/components/ProfileDetails";
+
+const Profile = () => {
+  return (
+    <>
+      <Nav />
+      <ProfileDetails />
+    </>
+  );
+};
+
+export default Profile;
