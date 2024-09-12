bca01fe771429f0cc92578278678296f9785cc22
ChrisShep98 <shepardchristopher1@gmail.com>
2024-09-11 22:09:12 -0400

    added loading.tsx to /song route

---
+++ src/app/song/[song]/loading.tsx
@@ -0,0 +1,9 @@
+import { Stack, Typography } from "@mui/material";
+
+export default function Loading() {
+  return (
+    <Stack alignItems={"center"} mt={5}>
+      <Typography>Loading...</Typography>
+    </Stack>
+  );
+}
