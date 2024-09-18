b728d6f057b44856d6b2d76c4b9a32846b878d42
ChrisShep98 <shepardchristopher1@gmail.com>
2024-09-18 13:43:20 -0400

    removed submission service functions and combined them into one to reflect backend changes

--- src/app/services/phishin.ts
+++ src/app/services/phishin.ts
@@ -46,14 +46,16 @@ export async function getAllPreformancesOfSongs(songSlug: string) {
   return dates;
 }
 
-export async function getAllSongSubmissions() {
-  const response = await fetch(`http://localhost:8000/allSubmissions`);
-  const allSubmissions = await response.json();
-  return allSubmissions.data;
-}
-
-export async function getSpecificSongSubmissions(slug: string) {
-  const response = await fetch(`http://localhost:8000/song/${slug}`);
-  const specificSongSubmissions = await response.json();
-  return specificSongSubmissions.data;
+export async function getSubmissions(filterName?: string, valueName?: string) {
+  if (filterName == undefined && valueName == undefined) {
+    const response = await fetch(`http://localhost:8000/submissions`);
+    const submissions = await response.json();
+    return submissions.data;
+  } else {
+    const response = await fetch(
+      `http://localhost:8000/submissions?filter=${filterName}&value=${valueName}`
+    );
+    const submissions = await response.json();
+    return submissions.data;
+  }
 }
