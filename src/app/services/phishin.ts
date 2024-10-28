bf026a76a645277238af56ec62c9aa94d8fa30e2
ChrisShep98 <shepardchristopher1@gmail.com>
2024-10-27 21:08:20 -0400

    removed unused old services

--- src/app/services/phishin.ts
+++ src/app/services/phishin.ts
@@ -1,29 +1,3 @@
-// import PHISH_KEY from "../lib/phishkey";
-
-export async function getShows(yearSelected: string) {
-  const response = await fetch(`https://phish.in/api/v1/years/${yearSelected}.json`, {
-    method: "GET",
-    headers: {
-      Authorization: process.env.PHISH_KEY!,
-      Accept: "application/json",
-    },
-  });
-  const shows = await response.json();
-  return shows;
-}
-
-export async function getTrackList(date: string) {
-  const response = await fetch(`https://phish.in/api/v1/shows/${date}.json`, {
-    method: "GET",
-    headers: {
-      Authorization: process.env.PHISH_KEY!,
-      Accept: "application/json",
-    },
-  });
-  const trackList = await response.json();
-  return trackList.data.tracks;
-}
-
 export async function getAllPreformancesOfSongs(songSlug: string) {
   const response = await fetch(`https://phish.in/api/v1/songs/${songSlug}.json`, {
     method: "GET",
